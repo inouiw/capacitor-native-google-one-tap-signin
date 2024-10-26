db8a3229ec297e63aa59e72e60b62711b65740f1
David Neuy <quantjump@gmail.com>
2024-10-26 22:01:47 +0200

    Update dependencies

---
+++ demo/react/vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+
+import legacy from '@vitejs/plugin-legacy'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy()
+  ],
+  server: {
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
+})
